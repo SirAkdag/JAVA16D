1fc7b54d8c0c73a39aab76692e8b0bc548d1b91f
SirAkdag <emreakdag@windowslive.com>
2023-07-14 21:04:59 +0200

    new project

---
+++ src/MiniDBMain.java
@@ -0,0 +1,9 @@
+//ACHTUNG!! Das Programm lässt sich nur ausführen, wenn derby.jar eingebunden ist
+
+public class MiniDBMain {
+	public static void main(String[] args) {
+		new MiniDBStart("MiniAddresse");
+	}
+
+
+}
